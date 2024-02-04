fd4666d1b4dc6df75e62463ee6ef96c496239158
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-02-05 00:40:03 +0100

    remove place

--- useEffect/src/App.jsx
+++ useEffect/src/App.jsx
@@ -49,6 +49,13 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -56,6 +63,12 @@ function App() {
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
     modal.current.close();
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    console.log(storedIds)
+    localStorage.setItem(
+      "selectedPlaces",
+      JSON.stringify(storedIds.filter(id=>id !== selectedPlace.current))
+    );
   }
 
   return (
