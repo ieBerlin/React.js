b976e5f8401d4062a10d32153849f1c74fe20cc3
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-02-07 12:53:14 +0100

    now this is about context only

--- class-based Component/src/components/User.js
+++ class-based Component/src/components/User.js
@@ -1,6 +1,9 @@
 import { Component } from "react";
 import classes from "./User.module.css";
 class User extends Component {
+  componentWillUnmount() {
+    console.log("Component unmounts from tree widget");
+  }
   render() {
     return <li className={classes.user}>{this.props.name}</li>;
   }
--- class-based Component/src/components/UserFinder.js
+++ class-based Component/src/components/UserFinder.js
@@ -1,5 +1,5 @@
 import { Fragment, Component } from "react";
-
+import UsersContext from "../store/users-context.js";
 import Users from "./Users.js";
 import classes from "./UserFinder.module.css";
 const DUMMY_USERS = [
@@ -8,13 +8,20 @@ const DUMMY_USERS = [
   { id: "u3", name: "Julie" },
 ];
 class UserFinder extends Component {
+  static contextType = UsersContext;
   searchChangeHandler = (event) => {
     this.setState({ searchTerm: event.target.value });
   };
+  componentDidMount() {
+    this.setState({
+      filteredUsers: this.context.users,
+    });
+  }
+
   componentDidUpdate(prevProps, prevState) {
     if (prevState.searchTerm !== this.state.searchTerm) {
       this.setState({
-        filteredUsers: DUMMY_USERS.filter((user) =>
+        filteredUsers: this.context.users.filter((user) =>
           user.name.includes(this.state.searchTerm)
         ),
       });
