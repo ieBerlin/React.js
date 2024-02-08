46b3dea3a416feeb0a1d23ca8da8de8b839f19d0
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-02-08 08:15:56 +0100

    Fetch data from API (Available Places)

--- Sending HTTP Requests/src/components/AvailablePlaces.jsx
+++ Sending HTTP Requests/src/components/AvailablePlaces.jsx
@@ -1,10 +1,21 @@
-import Places from './Places.jsx';
-
+import { useEffect } from "react";
+import Places from "./Places.jsx";
+import { useState } from "react";
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("http://localhost:3000/places");
+      const data = await response.json();
+      setAvailablePlaces(data["places"]);
+    }
+    fetchData();
+  }, []);
+
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
