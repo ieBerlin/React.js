8c1485a1a605e4487c147599a34e5a40279a2540
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-02-28 23:44:55 +0100

    Add a simple route for home page

--- Building a Multi-Pages-SPA with React Router/src/App.js
+++ Building a Multi-Pages-SPA with React Router/src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
