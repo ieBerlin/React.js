70ab4ee20b7bdf1976e90009bd0d4798d0bf0d7f
Alaa Eddine Ourmassi <aeourmassi@gmail.com>
2024-02-29 00:13:24 +0100

    Layout and nested Routes

--- Building a Multi-Pages-SPA with React Router/src/App.js
+++ Building a Multi-Pages-SPA with React Router/src/App.js
@@ -1,28 +1,35 @@
 import {
   createBrowserRouter,
   RouterProvider,
-  Route,
-  createRoutesFromElements,
+  // Route,
+  // createRoutesFromElements,
 } from "react-router-dom";
 import Home from "./pages/Home";
 import Products from "./pages/Products";
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Home />,
-//   },
-//   {
-//     path: "/products",
-//     element: <Products />,
-//   },
-// ]);
-const routerDefinitions = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<Home />} />
-    <Route path="/products" element={<Products />} />
-  </Route>
-);
-const router= createBrowserRouter(routerDefinitions);
+import RootLayout from "./pages/Root";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+    ],
+  },
+]);
+// const routerDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<Home />} />
+//     <Route path="/products" element={<Products />} />
+//   </Route>
+// );
+// const router= createBrowserRouter(routerDefinitions);
 function App() {
   return <RouterProvider router={router} />;
 }
--- Building a Multi-Pages-SPA with React Router/src/pages/Home.jsx
+++ Building a Multi-Pages-SPA with React Router/src/pages/Home.jsx
@@ -1,3 +1,11 @@
-export default function Home(){
-    return <h1>Welcome to Home Page!</h1>
+import { Link } from "react-router-dom";
+export default function Home() {
+  return (
+    <>
+      <h1>Welcome to Home Page!</h1>
+      <p>
+        <Link to="/products">Go to products page</Link>
+      </p>
+    </>
+  );
 }
